cc378606b04b325aa00f1b23825f6febfd523c6e
comconToukir <ahmdtoukir@gmail.com>
2022-09-03 23:42:14 +0600

    more minor adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ const loadCategories = data => {
   nav.classList.add("grid", "grid-cols-2", "sm:grid-cols-3", "md:grid-cols-4", "xl:grid-cols-8", "gap-2");
   data.forEach(category => {
     nav.innerHTML += `
-      <button id="category-${category.category_id}" class="category-btn text-left xl:text-center active:text-accent p-3">${category.category_name}</button>
+      <button id="category-${category.category_id}" class="category-btn text-left xl:text-center p-3">${category.category_name}</button>
     `
   });
   nav.children[0].classList.add("active");
@@ -53,18 +53,6 @@ const dataCheck = data => {
   }
 }
 
-// const getRatings = (data, id) => {
-//   if (typeof data !== "number") {
-//     return data;
-//   } else {
-//     const rating = Math.floor(((data % 1) * 2) + (parseInt(data) * 2));
-//     const ratingsDiv = document.getElementById("ratings-"+id);
-//     console.log(data, rating, ratingsDiv)
-//     // ratingsDiv.childNodes[rating].classList.add('checked');
-//   }
-// }
-
-
 // opens up modal with full news details
 const displayModal = async id => {
   try {
@@ -84,15 +72,13 @@ const displayModal = async id => {
       <div class="modal-overlay" id="modal-overlay">
         <div class="modal-div text-center w-5/6 md:w-3/5 xl:w-1/2 flex flex-col items-center bg-base-200">
           <div class="relative w-full">
-            <button class="badge-accent absolute right-3 top-3 py-4 px-6 rounded-xl font-bold" id="modal-close-btn">X</button>
+            <button class="btn-accent absolute right-3 top-3 py-4 px-6 rounded-xl font-bold" id="modal-close-btn">X</button>
             <img class="object-contain w-full mb-5" src=${news.image_url} alt=""/>
           </div>
           <div class="flex mb-4">
             ${news.others_info.is_todays_pick ? `<div class="badge badge-primary">Today's Pick</div>` : ""}
             ${news.others_info.is_trending ? `<div class="badge badge-accent">Trending</div>` : ""}
           </div>
-          <h2 class="mb-5 text-xl md:text-2xl xl:text-4xl">${news.title}</h2>
-          <p class="mb-7 font-light">Photo - ${news.details}</p>
           <div class="grid grid-cols-3 items-center mb-5">
             <div class="flex">
               <img class="author-img rounded-full mr-2" src='${dataCheck(news.author.img)}' alt="" />
@@ -104,8 +90,7 @@ const displayModal = async id => {
             <div class="mx-2">
               <p><i class="text-accent fa-regular fa-eye mr-2"></i>${dataCheck(news.total_view)}</p>
             </div>
-            <div class="rating rating-lg rating-half mx-2" id="ratings-${news._id}">
-              <input type="radio" name="rating-${news._id}" class="rating-hidden" />
+            <div class="rating rating-md rating-half mx-2" id="ratings-${news._id}">
               <input type="radio" name="rating-${news._id}" class="bg-accent ml-1 mask mask-star-2 mask-half-1" disabled />
               <input type="radio" name="rating-${news._id}" class="bg-accent mr-1 mask mask-star-2 mask-half-2" disabled />
               <input type="radio" name="rating-${news._id}" class="bg-accent ml-1 mask mask-star-2 mask-half-1" disabled />
@@ -118,6 +103,10 @@ const displayModal = async id => {
               <input type="radio" name="rating-${news._id}" class="bg-accent mr-1 mask mask-star-2 mask-half-2" disabled />
             </div>
           </div>
+          
+          <h2 class="mb-5 text-xl md:text-2xl xl:text-4xl">${news.title}</h2>
+          <p class="mb-7 font-light">Photo - ${news.details}</p>
+          
         </div>
       </div>
       `
@@ -159,8 +148,7 @@ const displayNewsList = newsArr => {
           <div class="mx-2 ml-auto lg:mx-auto">
             <p class=""><i class="text-accent fa-regular fa-eye mr-2"></i>${dataCheck(news.total_view)}</p>
           </div>
-          <div class="rating rating-lg rating-half mx-2" id="ratings-${news._id}">
-            <input type="radio" name="rating-${news._id}" class="rating-hidden" />
+          <div class="rating rating-md rating-half mx-2" id="ratings-${news._id}">
             <input type="radio" name="rating-${news._id}" class="bg-accent ml-1 mask mask-star-2 mask-half-1" disabled />
             <input type="radio" name="rating-${news._id}" class="bg-accent mr-1 mask mask-star-2 mask-half-2" disabled />
             <input type="radio" name="rating-${news._id}" class="bg-accent ml-1 mask mask-star-2 mask-half-1" disabled />
@@ -172,7 +160,7 @@ const displayNewsList = newsArr => {
             <input type="radio" name="rating-${news._id}" class="bg-accent ml-1 mask mask-star-2 mask-half-1" checked disabled />
             <input type="radio" name="rating-${news._id}" class="bg-accent mr-1 mask mask-star-2 mask-half-2" disabled />
           </div>
-          <button class="text-white bg-accent py-1 px-6 w-fit ml-auto rounded-md" onclick="displayModal('${news._id}')">Details</button>
+          <button class="text-white btn-accent py-1 px-6 w-fit ml-auto rounded-md" onclick="displayModal('${news._id}')">Details</button>
         </div>
       </div>
     </div>
